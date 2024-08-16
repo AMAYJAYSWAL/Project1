d9bb1511f0ef5a939745fb3455c317de472c799f
amay jayswal <amayjayswal@gmail.com>
2024-08-16 14:49:43 +0530

    basic setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  useFonts({
+    'Bodoni': require('./../assets/fonts/Regular.ttf'),
+    'Bodoni-Medium': require('./../assets/fonts/Medium.ttf'),
+    'Bodoni-Bold': require('./../assets/fonts/Bold.ttf'),
+    'Bodoni-ExtraBold': require('./../assets/fonts/ExtraBold.ttf')
+
+  })
   return (
-    <Stack>
+    <Stack screenOptions={{
+      headerShown:false
+    }}>
       <Stack.Screen name="(tabs)" />
+
     </Stack>
   );
 }
